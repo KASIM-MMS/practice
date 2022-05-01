c2f5829b05728bfc7a44b061ff148149eb6c489d
KASIM-MMS <parakletos.88@gmail.com>
2022-05-01 12:37:13 +0300

    Summary

---
+++ js/jscript.js
@@ -0,0 +1,10 @@
+// const totalSpent = 500;
+// let payment = 500;
+// let discount = 0;
+
+// if (totalSpent >= 100 && totalSpent < 1000) {
+//     console.log('Бронзовый партнер, скидка 2%')
+//     discount = 0.02;
+// }
+// console.log(`Оформление заказа на сумму ${payment} со скидкой ${discount * 100}%`);
+console.log(Number('5' - true));
